092a7d74b87819365f4716d4ee25429ef39d39f2
Anjali Jadhav <anjali.bhosle6@gmail.com>
2021-07-29 15:14:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ sc = joblib.load('hotel_scalar.pkl')
 def home():
     return render_template('index.html')
 
-@app.route('/y_predict',methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def y_predict():
     x_col = ['name',  'Check_in_year', 'Check_in_month', 'Check_in_day', 'Check_out_year', 'Check_out_month',
        'Check_out_day', 'city']
@@ -25,7 +25,7 @@ def y_predict():
     data = np.array([[x for x in request.form.values()]])
     data = sc.transform(data)
     
-    prediction = model.y_predict(data)
+    prediction = model.predict(data)
     
     print(prediction)
     return render_template('index.html',prediction_text=prediction)
