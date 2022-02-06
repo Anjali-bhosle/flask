de2f47a643a0b0a35159eed33d7eb41d1b4cc910
Anjali Jadhav <anjali.bhosle6@gmail.com>
2022-02-06 08:10:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 
 import numpy as np
 from flask import Flask,request,jsonify,render_template
-import  pandas as pd
-import joblib,pickle
-import logging
 import os
-import sys
+import  pandas as pd
+import joblib
 
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
-model = joblib.load('hotel.pkl')
-sc = joblib.load('hotel_scalar.pkl')
+model = joblib.load('xgb_regressor.pkl')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    x_col = ['name',  'Check_in_year', 'Check_in_month', 'Check_in_day', 'Check_out_year', 'Check_out_month',
-       'Check_out_day', 'city']
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    x_col = ['name', 'city', 'Distance to City centre(km)',
+       'Distance to Airport(km)', 'Pool', 'Free parking', 'Airport transfer',
+       'Spa', 'Restaurant', 'Gym', 'Bar', 'Bathtub', 'Meeting Facilities',
+       'Connecting rooms available', 'Pet-friendly', 'Kitchen',
+       'Internet access', 'Check_in_year', 'Check_in_month', 'Check_in_day',
+       'Check_out_year', 'Check_out_month', 'Check_out_day']
     
-    data = np.array([[x for x in request.form.values()]])
-    data = sc.transform(data)
+    data = [[x for x in request.form.values()]]
+    print(data)
+    
+    data = pd.DataFrame(data,columns=x_col)
     
     prediction = model.predict(data)
     
@@ -33,5 +34,3 @@ def predict():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    
-    
